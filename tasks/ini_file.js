71ae4248bca6f008c3528b9523d5984f0fe51801
Enrique Moreno <enriquemorenotent@gmail.com>
2014-01-18 22:25:16 +0000

    Added better syntax for the plugin

--- tasks/ini_file.js
+++ tasks/ini_file.js
@@ -12,26 +12,27 @@
 
 module.exports = function(grunt) {
 
-	grunt.registerTask('ini-file', 'Changing values to an INI file', function(key, newValue) {
+	grunt.registerMultiTask('ini-file', 'Changing values to an INI file', function() {
 
-		if (arguments.length !== 2) {
-			grunt.log.warn("Missing arguments");
-			return false;
-		}
+		var data = this.data;
 
-		var file = "tasks/test.ini";
+		var file = data.file;
 		if (!grunt.file.exists(file)) {
 			grunt.log.warn("File " + file + " does not exist");
 			return false;
 		}
 
-		var regex = new RegExp("(" + key +")( *)=( *)(.*)","g");
-		var src = grunt.file.read(file);
+		Object.keys(data.values).forEach(function(key) {
+			var newValue = data.values[key];
 
-		src = src.replace(regex, function(match, key, spacesBefore, spacesAfter, value){
-			return key + spacesBefore + "=" + spacesAfter + newValue;
+			var regex = new RegExp("(" + key +")( *)=( *)(.*)","g");
+			var src = grunt.file.read(file);
+
+			src = src.replace(regex, function(match, key, spacesBefore, spacesAfter, value){
+				return key + spacesBefore + "=" + spacesAfter + newValue;
+			});
+
+			grunt.file.write(file, src);
 		});
-
-		grunt.file.write(file, src);
 	});
 };
